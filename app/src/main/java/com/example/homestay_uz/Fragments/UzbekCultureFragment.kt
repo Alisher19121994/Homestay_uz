76f5b7b3f4255478f9ab943b00f528ba97baa2bc
student <alisherdaminov1912199427@gmail.com>
2022-11-12 15:38:34 +0500

    I've changed

--- app/src/main/java/com/example/homestay_uz/Fragments/UzbekCultureFragment.kt
+++ app/src/main/java/com/example/homestay_uz/Fragments/UzbekCultureFragment.kt
@@ -1,26 +1,57 @@
 package com.example.homestay_uz.Fragments
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.webkit.WebView
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.homestay_uz.R
+import com.example.homestay_uz.activities.SearchActivity
+
 class UzbekCultureFragment : Fragment() {
 
+    lateinit var googleSearch: LinearLayout
 
     companion object {
         fun newInstance() = UzbekCultureFragment()
     }
 
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_uzbek_culture, container, false)
+        val view = inflater.inflate(R.layout.fragment_uzbek_culture, container, false)
+        initViews(view)
+        return view
     }
+
+
+    private fun initViews(view: View) {
+        googleSearchPage(view)
+    }
+
+
+    private fun googleSearchPage(view: View){
+        googleSearch = view.findViewById(R.id.google_search_id)
+        googleSearch.setOnClickListener {
+            val intent = Intent(view.context,SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+
 
 
 }
--- app/src/main/java/com/example/homestay_uz/activities/SearchActivity.kt
+++ app/src/main/java/com/example/homestay_uz/activities/SearchActivity.kt
@@ -1,26 +1,46 @@
 package com.example.homestay_uz.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.webkit.WebView
 import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.homestay_uz.R
+import java.net.URL
 
 class SearchActivity : AppCompatActivity() {
-    lateinit var searchBackToMainPage: ImageView
+    lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    lateinit var webView: WebView
+    private var url = "https://www.google.com/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        setContentView(R.layout.google_search)
         initViews()
     }
 
     private fun initViews() {
-        searchBackToMainPage = findViewById(R.id.search_back_to_main_page)
-        searchBackToMainPage.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+        googleSearch()
+
+    }
+
+    @SuppressLint("WebViewApiAvailability", "SuspiciousIndentation")
+    private fun googleSearch() {
+
+        swipeRefreshLayout = findViewById(R.id.swipe_id)
+        webView = findViewById(R.id.webview_id)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            webView.webViewClient
         }
+
+        webView.loadUrl(url)
+        swipeRefreshLayout.isRefreshing = false
+
     }
 }
